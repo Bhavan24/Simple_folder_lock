2a4254438b1a096418be28968bb1977fdb93c66d
Loganathan Bhavaneetharan <68679905+Bhavan24@users.noreply.github.com>
2021-01-01 19:23:00 +0530

    Add files via upload

---
+++ Simple Folder Lock/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Simple_Folder_Lock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLock = new System.Windows.Forms.TextBox();
+            this.txtUnlock = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fbdL = new System.Windows.Forms.FolderBrowserDialog();
+            this.fbdU = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Night Machine", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(28, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lock Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Night Machine", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(28, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Unlock Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("BankGothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder Path";
+            // 
+            // txtLock
+            // 
+            this.txtLock.Enabled = false;
+            this.txtLock.Location = new System.Drawing.Point(139, 45);
+            this.txtLock.Name = "txtLock";
+            this.txtLock.Size = new System.Drawing.Size(346, 20);
+            this.txtLock.TabIndex = 2;
+            // 
+            // txtUnlock
+            // 
+            this.txtUnlock.Enabled = false;
+            this.txtUnlock.Location = new System.Drawing.Point(139, 122);
+            this.txtUnlock.Name = "txtUnlock";
+            this.txtUnlock.Size = new System.Drawing.Size(346, 20);
+            this.txtUnlock.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("BankGothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Folder Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(523, 160);
+            this.Controls.Add(this.txtUnlock);
+            this.Controls.Add(this.txtLock);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Simple Folder Lock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLock;
+        private System.Windows.Forms.TextBox txtUnlock;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog fbdL;
+        private System.Windows.Forms.FolderBrowserDialog fbdU;
+    }
+}
+
---
+++ Simple Folder Lock/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Simple_Folder_Lock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x = ".{2559a1f2-21d7-11d4-bdaf-00c04f60b9f0}";
+            if (fbdL.ShowDialog() == DialogResult.OK)
+            {
+                txtLock.Text = fbdL.SelectedPath;
+
+                DirectoryInfo d = new DirectoryInfo(fbdL.SelectedPath);
+                string selectedpath = d.Parent.FullName + d.Name;
+                if (fbdL.SelectedPath.LastIndexOf(".{") == -1)
+                {
+                    if (!d.Root.Equals(d.Parent.FullName))
+                        d.MoveTo(d.Parent.FullName + "\\" + d.Name + x);
+                    else d.MoveTo(d.Parent.FullName + d.Name + x);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select Path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fbdU.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo d = new DirectoryInfo(fbdU.SelectedPath);
+                string Selectedpath = d.Parent.FullName + d.Name;
+
+                txtUnlock.Text = fbdU.SelectedPath;
+
+                d.MoveTo(fbdU.SelectedPath.Substring(0, fbdU.SelectedPath.LastIndexOf(".")));
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select Path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
